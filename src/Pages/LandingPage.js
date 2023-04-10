1c18974bfd71821b0989d05e95e9fb358a695d6b
akacoder404 <akacoder404@outlook.com>
2023-04-10 11:18:03 +0800

    update landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button } from 'antd';
+import { Button } from "antd";
 import cover from "../assets/images/cover_with_text5.png";
-import { FaSchool } from 'react-icons/fa';
-import { BiMath } from 'react-icons/bi';
-import { MdSchool } from 'react-icons/md';
+import { FaSchool } from "react-icons/fa";
+import { BiMath } from "react-icons/bi";
+import { MdSchool } from "react-icons/md";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
 
-import './LandingPage.css';
+import "./LandingPage.css";
 
 function formatTimeValue(value) {
-    if (value < 10) {
-      return `0${value}`;
-    } else {
-      return value.toString();
+  if (value < 10) {
+    return `0${value}`;
+  } else {
+    return value.toString();
   }
 }
 
@@ -22,24 +23,25 @@ function formatTimeValue(value) {
 // const minutes = "00";
 // const seconds = "00";
 function LandingPage() {
-    const [days, setDays] = React.useState("00");
-    const [hours, setHours] = React.useState("00");
-    const [minutes, setMinutes] = React.useState("00");
-    const [seconds, setSeconds] = React.useState("00");
+  const [days, setDays] = React.useState("00");
+  const [hours, setHours] = React.useState("00");
+  const [minutes, setMinutes] = React.useState("00");
+  const [seconds, setSeconds] = React.useState("00");
 
-    const [isRegisterButtonEnabled, setIsRegisterButtonEnabled] = React.useState(true);
+  const [isRegisterButtonEnabled, setIsRegisterButtonEnabled] =
+    React.useState(true);
 
-    let navigate = useNavigate();
-    // Somewhere in your code, e.g. inside a handler:
-  
-    // Function after clicking the register button
-    const onRegisterClick = () => {
-        console.log("Register button clicked");
-        navigate("/postersession/register"); 
-    }
+  let navigate = useNavigate();
+  // Somewhere in your code, e.g. inside a handler:
 
-    useEffect(() => {
-          // Calculate the time remaining until the target date
+  // Function after clicking the register button
+  const onRegisterClick = () => {
+    console.log("Register button clicked");
+    navigate("/postersession/register");
+  };
+
+  useEffect(() => {
+    // Calculate the time remaining until the target date
     const targetDate = new Date("2023-04-24T00:00:00");
     const now = new Date();
     const timeDiff = targetDate.getTime() - now.getTime();
@@ -54,9 +56,13 @@ function LandingPage() {
     remainingTime -= remainingMinutes * 60;
     const remainingSeconds = Math.floor(remainingTime);
 
-
     // Stop the interval when the countdown reaches zero
-    if (remainingDays < 0 || remainingHours < 0 || remainingMinutes < 0 || remainingSeconds < 0) {
+    if (
+      remainingDays < 0 ||
+      remainingHours < 0 ||
+      remainingMinutes < 0 ||
+      remainingSeconds < 0
+    ) {
       // clearInterval(intervalId);
       setIsRegisterButtonEnabled(false);
       setDays(formatTimeValue(0));
@@ -72,8 +78,6 @@ function LandingPage() {
     setMinutes(formatTimeValue(remainingMinutes));
     setSeconds(formatTimeValue(remainingSeconds));
 
-   
-
     // Set up the interval to update the countdown every second
     const intervalId = setInterval(() => {
       // Calculate the new values for days, hours, minutes, and seconds
@@ -81,7 +85,7 @@ function LandingPage() {
       let newMinutes = minutes;
       let newHours = hours;
       let newDays = days;
-      
+
       if (newSeconds < 0) {
         newSeconds = 59;
         newMinutes--;
@@ -102,7 +106,12 @@ function LandingPage() {
       setSeconds(newSeconds);
 
       // Stop the interval when the countdown reaches zero
-      if (newDays === 0 && newHours === 0 && newMinutes === 0 && newSeconds === 0) {
+      if (
+        newDays === 0 &&
+        newHours === 0 &&
+        newMinutes === 0 &&
+        newSeconds === 0
+      ) {
         clearInterval(intervalId);
       }
     }, 1000);
@@ -111,77 +120,131 @@ function LandingPage() {
     return () => clearInterval(intervalId);
   }, [days, hours, minutes, seconds]);
 
+  const requirementsIcon = {
+    marginTop: "40px",
+    fontSize: "50px",
+  };
 
-    const requirementsIcon = {
-        marginTop: "40px",
-        fontSize: "50px",
-    }
-
-    return (
-        <div className="LandingPage">
-            <div className="cover">
-                {/* <div className='cover-date'> March 27 - April 9, 2023 </div>
+  return (
+    <div className="LandingPage">
+      <div className="cover">
+        {/* <div className='cover-date'> March 27 - April 9, 2023 </div>
                 <div className="cover-title"> POSTER SESSION </div>
                 <div className="cover-subtitle"> International Congress of Basic Science </div> */}
-                <img id="cover" src={cover} alt="cover" style={{ width: '100%' }} />
-            </div>
-            <div className="countdown">
-                <div className='countdown-divider'></div>
-                <div className='countdown-time'>
-                    <div className='countdown-timer'> {days} </div>
-                    <div className='countdown-label'> days </div>
-                </div>
-                <div className='countdown-divider'></div>
-                <div className='countdown-time'>
-                    <div className='countdown-timer'> {hours} </div>
-                    <div className='countdown-label'> hours </div>
-                </div>
-                <div className='countdown-divider'></div>
-                <div className='countdown-time'>
-                    <div className='countdown-timer'> {minutes} </div>
-                    <div className='countdown-label'> minutes </div>
-                </div>
-                <div className='countdown-divider'></div>
-                <div className='countdown-time'>
-                    <div className='countdown-timer'> {seconds} </div>
-                    <div className='countdown-label'> seconds </div>
-                </div>
-                <div className='countdown-divider'></div>
-                <Button 
-                    // type="primary" 
-                    size="large" 
-                    className="countdown-button" 
-                    disabled={!isRegisterButtonEnabled}
-                    onClick={onRegisterClick}>
-                        REGISTER</Button>
-            </div>
-
-            <div className="LandingPage-info">
-                <div className="LandingPage-info-subtitle"> Poster Session </div>
-                <div className="LandingPage-info-title"> <div className="LandingPage-info-title-layout"><span style={{color: "#004DA6"}}>ABOUT</span>{'\u00A0'}THE POSTER SESSION </div></div>
-                <div className="LandingPage-info-text"> A half day poster session will be held during the inaugural International Congress of Basic Science (ICBS). This is an opportunity for outstanding undergraduate and graduate students worldwide to display and introduce their recent works, to communicate and discuss with various fields of mathematicians and scholars. The congress will provide up to 25000RMB economic airfare support and accommodation (shared room) for every successful applicant. </div>
-            </div>
-
-            <div className="LandingPage-requirement">
-                <div className="LandingPage-requirement-title"> REQUIREMENTS </div>
-                <div className='LandingPage-requirements'>
-                    <div className="LandingPage-info-requirement">  
-                      <div className="LandingPage-info-requirement-icon"> <BiMath style={requirementsIcon}/> </div>
-                      <div className="LandingPage-info-requirement-title"> Major in Mathematics and its interdisciplinary subjects. </div>
-                    </div>
-                    <div className="LandingPage-info-requirement">  
-                      <div className="LandingPage-info-requirement-icon"> <FaSchool style={requirementsIcon}/> </div>
-                      <div className="LandingPage-info-requirement-title"> Worldwide outstanding undergraduate and graduate students with affiliations. </div>
-                     </div>
-
-                    <div className='LandingPage-info-requirement'>  
-                      <div className="LandingPage-info-requirement-icon"> <MdSchool style={requirementsIcon} /> </div>
-                      <div className="LandingPage-info-requirement-title"> Juvenile under 18 will not be considered. </div>
-                    </div>
-                </div>
-            </div>
+        <img id="cover" src={cover} alt="cover" style={{ width: "100%" }} />
+      </div>
+      <div className="countdown">
+        <div className="countdown-divider"></div>
+        <div className="countdown-time">
+          <div className="countdown-timer"> {days} </div>
+          <div className="countdown-label"> days </div>
         </div>
-    );
+        <div className="countdown-divider"></div>
+        <div className="countdown-time">
+          <div className="countdown-timer"> {hours} </div>
+          <div className="countdown-label"> hours </div>
+        </div>
+        <div className="countdown-divider"></div>
+        <div className="countdown-time">
+          <div className="countdown-timer"> {minutes} </div>
+          <div className="countdown-label"> minutes </div>
+        </div>
+        <div className="countdown-divider"></div>
+        <div className="countdown-time">
+          <div className="countdown-timer"> {seconds} </div>
+          <div className="countdown-label"> seconds </div>
+        </div>
+        <div className="countdown-divider"></div>
+        <Button
+          // type="primary"
+          size="large"
+          className="countdown-button"
+          disabled={!isRegisterButtonEnabled}
+          onClick={onRegisterClick}
+        >
+          REGISTER
+        </Button>
+      </div>
+
+      <div className="RegisterPage-rightColumn-form-callout" style={{alignContent: "center"}}>
+        <div className="callout-col">
+          <div className="callout-icon">
+            <ExclamationCircleOutlined />
+          </div>
+          <div className="callout-text">
+            The registration deadline has been postponed to the 24:00, 23rd
+            April, 2023 for students with Chinese nationality or foreign
+            students who have obtained a Chinese visa
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="LandingPage-notice">
+        The registration deadline has been postponed to the 24:00, 23rd April,
+        2023 for students with Chinese nationality or foreign students who have
+        obtained a Chinese visa
+      </div> */}
+
+      <div className="LandingPage-info">
+        <div className="LandingPage-info-subtitle"> Poster Session </div>
+        <div className="LandingPage-info-title">
+          {" "}
+          <div className="LandingPage-info-title-layout">
+            <span style={{ color: "#004DA6" }}>ABOUT</span>
+            {"\u00A0"}THE POSTER SESSION{" "}
+          </div>
+        </div>
+        <div className="LandingPage-info-text">
+          {" "}
+          A half day poster session will be held during the inaugural
+          International Congress of Basic Science (ICBS). This is an opportunity
+          for outstanding undergraduate and graduate students worldwide to
+          display and introduce their recent works, to communicate and discuss
+          with various fields of mathematicians and scholars. The congress will
+          provide up to 25000RMB economic airfare support and accommodation
+          (shared room) for every successful applicant.{" "}
+        </div>
+      </div>
+
+      <div className="LandingPage-requirement">
+        <div className="LandingPage-requirement-title"> REQUIREMENTS </div>
+        <div className="LandingPage-requirements">
+          <div className="LandingPage-info-requirement">
+            <div className="LandingPage-info-requirement-icon">
+              {" "}
+              <BiMath style={requirementsIcon} />{" "}
+            </div>
+            <div className="LandingPage-info-requirement-title">
+              {" "}
+              Major in Mathematics and its interdisciplinary subjects.{" "}
+            </div>
+          </div>
+          <div className="LandingPage-info-requirement">
+            <div className="LandingPage-info-requirement-icon">
+              {" "}
+              <FaSchool style={requirementsIcon} />{" "}
+            </div>
+            <div className="LandingPage-info-requirement-title">
+              {" "}
+              Worldwide outstanding undergraduate and graduate students with
+              affiliations.{" "}
+            </div>
+          </div>
+
+          <div className="LandingPage-info-requirement">
+            <div className="LandingPage-info-requirement-icon">
+              {" "}
+              <MdSchool style={requirementsIcon} />{" "}
+            </div>
+            <div className="LandingPage-info-requirement-title">
+              {" "}
+              Juvenile under 18 will not be considered.{" "}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
